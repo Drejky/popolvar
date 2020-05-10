ee82e66b9a0b1b58cce159536815d2f9f2ac0cfa
Drejky <peter.kudela9@gmail.com>
2020-05-10 03:38:58 +0200

    ...

--- main.c
+++ main.c
@@ -12,6 +12,7 @@ typedef struct graf {
 	//Variables needed for dijkstra
 	int distance;
 	int visited;
+	int heaped;
 	struct graf** neighbours;
 	struct graf* prev;
 
@@ -31,6 +32,7 @@ Node* newNode(int x, int y, int cost) {
 
 	temp->distance = 10000; //Infinity
 	temp->visited = 0;
+	temp->heaped = 0;
 	temp->neighbours = (Node**)calloc(4, sizeof(Node));
 	temp->prev = NULL;
 	return temp;
@@ -55,32 +57,42 @@ Node** heap_pop(Node** heap, int *size) {
 		return NULL;
 	else if (*size == 1) {
 		*size = 0;
-		free(heap[0]);
-		return NULL;
+		heap[0] = NULL;
+		return heap;
 	}
-	free(heap[0]);
 	heap[0] = heap[*size - 1];
 	heap[*size - 1] = NULL;
 	(*size)--;
 	int curr = 0;
 	while(1){
-		if (getVal(heap[2 * curr]) <= getVal(heap[2 * curr + 1]) && getVal(heap[curr]) > getVal(heap[2 * curr])) {
-			Node* temp = heap[curr];
-			heap[curr] = heap[2 * curr];
-			heap[2 * curr] = temp;
-			curr *= 2;
+		if (2 * curr + 1 < *size) {
+			if (getVal(heap[2 * curr]) <= getVal(heap[2 * curr + 1]) && getVal(heap[curr]) > getVal(heap[2 * curr])) {
+				Node* temp = heap[curr];
+				heap[curr] = heap[2 * curr];
+				heap[2 * curr] = temp;
+				curr *= 2;
+			}
+			else if (getVal(heap[2 * curr]) > getVal(heap[2 * curr + 1]) && getVal(heap[curr]) > getVal(heap[2 * curr + 1])) {
+				Node* temp = heap[curr];
+				heap[curr] = heap[2 * curr + 1];
+				heap[2 * curr + 1] = temp;
+				curr *= 2;
+				curr++;
+			}
+			else
+				break;
 		}
-		else if (getVal(heap[2 * curr]) > getVal(heap[2 * curr + 1]) && getVal(heap[curr]) > getVal(heap[2 * curr + 1])) {
-			Node* temp = heap[curr];
-			heap[curr] = heap[2 * curr + 1];
-			heap[2 * curr + 1] = temp;
-			curr *= 2;
-			curr++;
-		}
-		else
+		else 
 			break;
 	}
 	return heap;
+}
+
+int factorial(int x) {
+	int res = x;
+	while (x > 1) 
+		res *= --x;
+	return res;
 }
 
 int isWalkable(char x) {
@@ -122,51 +134,177 @@ Node* getNeighbours(Node* node, Node** graph, int n, int m) {
 	return node;
 }
 
-Node* dijkstra(Node* curr, Node* prev, Node* goal, Node*** heap, int *heap_size) {
-	curr->visited = 1;
-	for (int i = 0; i < 4; i++) {
-		if (curr->neighbours[i] && !curr->neighbours[i]->visited) {
-			heap_ins(*heap, curr->neighbours[i], heap_size);
-			if (curr->distance + curr->neighbours[i]->cost < curr->neighbours[i]->distance) {
-				curr->neighbours[i]->distance = curr->distance + curr->neighbours[i]->cost;
-				curr->neighbours[i]->prev = curr;
+int* dijkstra(Node* curr, Node* end, int n, int m, int *len) {
+	Node** heap = (Node**)malloc(n * m * sizeof(Node*));
+	int heap_size = 0;
+	curr->distance = 1;
+
+	while (1) {
+		curr->visited = 1;
+		curr->heaped = 0;
+		for (int i = 0; i < 4; i++) {
+			if (curr->neighbours[i] && curr->neighbours[i]->visited == 0) {
+				//Relax
+				if (curr->neighbours[i]->cost + curr->distance < curr->neighbours[i]->distance) {
+					curr->neighbours[i]->distance = curr->neighbours[i]->cost + curr->distance;
+					curr->neighbours[i]->prev = curr;
+				}
+				if (curr->neighbours[i]->heaped == 0) {
+					curr->neighbours[i]->heaped = 1;
+					heap = heap_ins(heap, curr->neighbours[i], &heap_size);
+				}
 			}
 		}
-
+		curr = heap[0];
+		//showHeap(heap, heap_size);
+		heap = heap_pop(heap, &heap_size);
+		if (heap == NULL)
+			return NULL;
+		if (curr->x == end->x && curr->y == end->y )
+			break;
+		/*
+		printf("\nafter--------------------\n");
+		showHeap(heap, heap_size);
+		printf("==================\n");*/
 	}
-	return curr;
+	free(heap);
+	Node* temp = end;
+	*len = 0;
+	while (temp) {
+		temp = temp->prev;
+		(*len)++;
+	}
+	temp = end;
+	int* coordinates_array = (int*)calloc(sizeof(int), 2 * (*len));
+	int counter = (*len) *2 - 1;
+	while (temp) {
+		coordinates_array[counter--] = temp->y;
+		coordinates_array[counter--] = temp->x;
+		temp = temp->prev;
+	}
+
+	return coordinates_array;
+}
+
+Node** clear(Node** graph, int len) {
+	for (int i = 0; i < len; i++) {
+		graph[i]->prev = NULL;
+		graph[i]->heaped = NULL;
+		graph[i]->visited = 0;
+		graph[i]->distance = 10000;
+	}
+	return graph;
+}
+
+int* getPerm(int** pLens, int p_count, int from) {
+	int min = 10000, index;
+	int* perm = (int*)calloc(p_count + 2, sizeof(int));
+	perm[0] = 0;
+	for (int j = 1; j <= p_count; j++) {
+		for (int i = 1; i < p_count + 1; i++) {
+			if (pLens[from][i] < min) {
+				min = pLens[from][i];
+				index = i;
+			}
+		}
+		perm[j] = index;
+		for (int i = 1; i < p_count + 1; i++) {
+			pLens[i][index] = 10000;
+		}
+		from = index;
+		min = 10000;
+	}
+	return perm;	
 }
 
 int *zachran_princezne(char **mapa, int n, int m, int t, int *dlzka_cesty) {
-	Node** heap = (Node**)malloc(n * m * sizeof(Node*));
-	int heap_size = 0;
 	Node** graph = (Node**)malloc(n* m * sizeof(Node*));
 	//Stuff popolvar needs to find
-	int dragon;
-	int princ[5];
-	int p_count = 0;
+	int *goals = (int*)calloc(6, sizeof(int));	//Dragon and princesses
+	int p_count = 1;	//Ammount of princesses
 	
 	//Graph representation
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			graph[i * m + j] = newNode(j, i, isWalkable(mapa[i][j]));
 			if (mapa[i][j] == 'D')
-				dragon = i * m + j;
+				goals[0] = i * m + j;
 			else if (mapa[i][j] == 'P')
-				princ[p_count++] = i * m + j;
+				goals[p_count++] = i * m + j;
 		}
 	}
-	graph[0]->distance = 0;
+	p_count--;
 
 	//Gets neighbours for all nodes ignoring N tiles
 	for (int i = 0; i < n*m; i++)
 		graph[i] = getNeighbours(graph[i], graph, n, m);
 	
+	//Finds dragon
+	int len;
+	int *path;
+	path = dijkstra(graph[0], graph[goals[0]], n, m, &len);
+	graph = clear(graph, n*m);
+	
+	//Finds all possible paths between princesses
+	//Allocations
+	int **pLens = (int**)calloc(1 + p_count, sizeof(int*));
+	int ***pPaths = (int***)calloc(1 + p_count, sizeof(int**));
+	for (int i = 0; i < 1 + p_count; i++) {
+		pPaths[i] = (int**)calloc(1 + p_count, sizeof(int*));
+		pLens[i] = (int*)calloc(1 + p_count, sizeof(int));
+	}
+	
+	//Finding the paths
+	for (int i = 0; i < p_count + 1; i++) {
+		for (int j = 0; j < p_count + 1; j++) {
+			if (i == j) {
+				pLens[i][j] = 10000;
+				continue;
+			}
+			pPaths[i][j] = dijkstra(graph[goals[i]], graph[goals[j]], n, m, &pLens[i][j]);
+			graph = clear(graph, n*m);
+		}
+	}
 
-	//Debuging
-	graph[0] = dijkstra(graph[0], NULL, NULL, &heap, &heap_size);
-	printf("DEBUG %d",graph[5]->distance);
-
+	//Find an acceptable path
+	int** dummy_pLens = (int**)calloc(1 + p_count, sizeof(int*));
+	for (int i = 0; i < 1 + p_count; i++) {
+		dummy_pLens[i] = (int*)calloc(1 + p_count, sizeof(int));
+		for (int j = 0; j < 1 + p_count; j++) {
+			dummy_pLens[i][j] = pLens[i][j];
+		}
+	}
+	int *perm = getPerm(dummy_pLens, p_count, 0);
+	
+	for (int i = 0; i < 2 * len; i += 2) {
+		printf("%d %d\n", path[i], path[i + 1]);
+	}
+	for (int i = 0; i < p_count; i++) {
+		for (int j = 2; j < pLens[perm[i]][perm[i + 1]] * 2; j += 2) {
+			printf("%d %d\n", pPaths[perm[i]][perm[i + 1]][j], pPaths[perm[i]][perm[i + 1]][j + 1]);
+		}	
+	}
+	
+	//Free pPaths
+	
+	for (int i = 0; i < 1 + p_count; i++) {
+		for (int j = 0; j < 1 + p_count; j++) {
+			free(pPaths[i][j]);
+		}
+		free(pPaths[i]);
+	}
+	free(pPaths);
+	
+	//Free pLens
+	for (int i = 0; i < 1 + p_count; i++) {
+		free(pLens[i]);
+	}
+	free(pLens);
+	//Free the rest
+	for (int i = 0; i < n*m; i++)
+		free(graph[i]);
+	free(graph);
+	free(goals);
 
 	return NULL;
 }
@@ -186,7 +324,7 @@ int main()
 		case 0://ukonci program
 			return 0;
 		case 1://nacitanie mapy zo suboru
-			f = fopen("C:\\Users\\user\\source\\repos\\Popolvar\\text.txt", "r");
+			f = fopen("C:\\Users\\user\\source\\repos\\Popolvar\\text2.txt", "r");
 			if (f)
 				fscanf(f, "%d %d %d", &n, &m, &t);
 			else
